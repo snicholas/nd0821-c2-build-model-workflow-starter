a10a53fef3af8b5e3929ca3c040d9d4279a9c09d
Nicholas Spadaro <spadaro.nicholas@gmail.com>
2022-03-04 00:10:40 +0100

    Fix out of boundaries geolocation data

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,6 +29,9 @@ def go(args):
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("converting last_review date from str to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
     logger.info("save cleaned dataset to disk")
